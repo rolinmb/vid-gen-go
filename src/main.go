8bd64a4cc11e0512d1bd990439e5ef22a1269385
Rolin Blake <rolinmblake@gmail.com>
2023-11-26 06:49:33 -0600

    README updated so I could remove comments in main.go for template functions

--- src/main.go
+++ src/main.go
@@ -75,48 +75,7 @@ func clamp(value,min,max int) int {
 }
 
 func getPixelColorOne(x,y,WIDTH,HEIGHT int, SCALE,COMPL,CLRFACTOR float64) (uint8, uint8, uint8) {
-    // angle := (math.Pi * SCALE * math.Sin(float64(x*x*y/5))) // Change to +/-/* or divide/modulus by x+1 or y+1
-    // angle := math.Pi * SCALE * (math.Cos(float64(x+y))*0.2)
-    // angle := math.Pi * SCALE * math.Tan(float64(x+(y/((x*y)+1))) - math.Sin(float64(x*y)*0.1))
-    // angle := math.Pi * 2.0 * SCALE *  math.Sqrt(float64(x*x+y*y)) // circular gradient
-    // angle := math.Pi * SCALE * math.Sin(float64(x)*0.1) + math.Pi * SCALE * math.Sin(float64(y)*0.1) // sine wave ripple
-    // angle := math.Pi * SCALE * (1 / (float64(x)*float64(x) + float64(y)*float64(y) + 1))  // hyperbolic spiral
-    // angle := math.Pi * SCALE * float64(x*x+y*y)  // square
-    // angle := math.Pi * SCALE * math.Exp(-0.01 * math.Sqrt(float64(x*x+y*y)))  // exponential decay
-    // angle := math.Pi * SCALE * math.Sin(0.1*float64(x)+0.1*float64(y)) + math.Pi/4  // offset sine wave
-    angle := math.Pi * SCALE * math.Sin(3*float64(x)) + math.Pi * SCALE * math.Sin(4*float64(y))  // lissajous curve
-    // angle := math.Pi * SCALE * math.Cos(3*float64(x*y)) + math.Pi * SCALE * math.Sin(4*float64(y))
-    // angle := math.Pi * SCALE * math.Abs(float64(x)-WIDTH/2) + math.Pi * SCALE * math.Abs(float64(y)-HEIGHT/2) // diamond
-    // angle := math.Pi * SCALE * (float64(x)/10 + float64(y)/5) + math.Pi * SCALE * math.Sin(2*float64(x))  // hypotochoid
-    // angle := math.Pi * SCALE * (math.Sin(float64(x)*0.05) * math.Exp(-float64(y)*0.1))
-    // Remix Set 1 Generators
-    //  centerX := float64(WIDTH) / 2
-    //  centerY := float64(HEIGHT) / 2
-    /*angle := math.Atan2(float64(y)-centerY, float64(x)-centerX)
-    angle = angle + COMPL*math.Sin(angle)
-    r := uint8((math.Sin(angle) + 1) * 127.5 * CLRFACTOR)
-    g := uint8((math.Sin(angle+2*math.Pi/3) + 1) * 127.5 * CLRFACTOR)
-    b := uint8((math.Sin(angle+4*math.Pi/3) + 1) * 127.5 * CLRFACTOR)*/
-    /*distance := math.Sqrt(math.Pow(float64(x)-centerX, 2) + math.Pow(float64(y)-centerY, 2))
-    distance = distance + COMPL*math.Sin(distance*0.1)
-    r := uint8((math.Sin(distance*0.1) + 1) * 127.5 * CLRFACTOR)
-    g := uint8((math.Sin(distance*0.1+2*math.Pi/3) + 1) * 127.5 * CLRFACTOR)
-    b := uint8((math.Sin(distance*0.1+4*math.Pi/3) + 1) * 127.5 * CLRFACTOR)*/
-    /*angleX := math.Pi * COMPL * (float64(x) - centerX) / centerX
-    angleY := math.Pi * COMPL * (float64(y) - centerY) / centerY
-    angle := math.Sin(angleX) + math.Cos(angleY)
-    angle = angle + COMPL*math.Sin(angle*10)
-    r := uint8((math.Sin(angle) + 1) * 127.5 * CLRFACTOR)
-    g := uint8((math.Sin(angle+2*math.Pi/3) + 1) * 127.5 * CLRFACTOR)
-    b := uint8((math.Sin(angle+4*math.Pi/3) + 1) * 127.5 * CLRFACTOR)*/
-    /*distance := math.Sqrt(math.Pow(float64(x)-centerX, 2) + math.Pow(float64(y)-centerY, 2))
-    angleX := math.Pi * COMPL * (float64(x) - centerX) / centerX
-    angleY := math.Pi * COMPL * (float64(y) - centerY) / centerY
-    combinedAngle := angleX + angleY + distance
-    trippyAngle := combinedAngle + COMPL*math.Sin(combinedAngle)
-    r := uint8((math.Sin(trippyAngle) + 1) * 127.5 * CLRFACTOR)
-    g := uint8((math.Sin(trippyAngle+2*math.Pi/3) + 1) * 127.5 * CLRFACTOR)
-    b := uint8((math.Sin(trippyAngle+4*math.Pi/3) + 1) * 127.5 * CLRFACTOR)*/
+    angle := math.Pi * SCALE * math.Sin(3*float64(x)) + math.Pi * SCALE * math.Sin(4*float64(y))
     distance := math.Sqrt(math.Pow(float64(x-WIDTH/2), 2) + math.Pow(float64(y-HEIGHT/2), 2))
 	frequency := distance * SCALE
     r := uint8(math.Sin(angle * COMPL + frequency) * CLRFACTOR + 128)
@@ -186,25 +145,7 @@ func routineSimple(
 }
 
 func getPixelColorTwo(x,y,WIDTH,HEIGHT int, SCALE,COMPL,CLRFACTOR float64) (uint8, uint8, uint8) {
-	// angle := math.Pi * SCALE * (float64(x)/10 + float64(y)/5) + math.Pi * SCALE * math.Sin(2*float64(x))  // hypotochoid
-	// angle := math.Pi * SCALE * math.Sin(3*float64(x)) + math.Pi * SCALE * math.Sin(4*float64(y)) // lissajous curve
-	// angle := math.Pi * SCALE * math.Sin(0.1*float64(x)+0.1*float64(y)) + math.Pi/4  // offset sine wave
-	// angle := math.Pi * SCALE * math.Sin(float64(x)*0.1) + math.Pi * SCALE * math.Sin(float64(y)*0.1) // sine wave ripple
-	// angle := math.Pi * SCALE * math.Abs(float64(x)-WIDTH/2) + math.Pi * SCALE * math.Abs(float64(y)-HEIGHT/2) // diamond
-	// angle := math.Pi * SCALE * (1 / (float64(x)*float64(x) + float64(y)*float64(y) + 1))  // hyperbolic spiral
-	// angle := math.Pi * SCALE * math.Exp(-0.01 * math.Sqrt(float64(x*x+y*y)))  // exponential decay
-	// angle := math.Pi * 2.0 * SCALE *  math.Sqrt(float64(x*x+y*y)) // circular gradient
-	// angle := math.Pi * SCALE * math.Cos(3*float64(x*y)) + math.Pi * SCALE * math.Sin(4*float64(y))
     angle := math.Pi * SCALE * ((math.Sin(float64(x)*0.1) + math.Sin(float64(y)*0.1)) / (1 + math.Sqrt(float64(x*x+y*y))))
-	// angle := math.Pi * 2.0 * SCALE *  math.Sqrt(float64(x*x+y*y))
-	/*// Remix Set 1 Generators
-    centerX := float64(WIDTH) / 2
-    centerY := float64(HEIGHT) / 2
-    angle := math.Atan2(float64(x*y)-centerY, float64(x*x*y*y)-centerX)
-    // angle = angle + COMPL*math.Sin(angle)
-    r := uint8((math.Sin(angle) + 1) * 127.5 * CLRFACTOR)
-    g := uint8((math.Sin(angle+2*math.Pi/3) + 1) * 127.5 * CLRFACTOR)
-    b := uint8((math.Sin(angle+4*math.Pi/3) + 1) * 127.5 * CLRFACTOR)*/
 	distance := math.Sqrt(math.Pow(float64(x-WIDTH/2), 2) + math.Pow(float64(y-HEIGHT/2), 2))
 	frequency := distance * SCALE
     r := uint8(math.Sin(angle * COMPL + frequency) * CLRFACTOR + 128)
