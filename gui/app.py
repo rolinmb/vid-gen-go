1296c65322c649500aebea92aab0a670b7ebfca3
Rolin Blake <rolinmblake@gmail.com>
2024-05-12 19:43:22 -0500

    adding more to gui

--- gui/app.py
+++ gui/app.py
@@ -8,6 +8,15 @@ class App:
         self.root = root
         self.root.title("vid-gen-go")
         self.root.geometry("720x720")
+        self.vid_in_label = tk.Label(self.root, width=100, height=1, text="video input name (from src/vid_in)", fg="black")
+        self.vid_in_label.grid(row=0)
+        # TODO: make a selection list of video names in src/vid_in instead of having to enter exact vid name
+        self.vid_in = tk.Text(self.root, width=100, height=1)
+        self.vid_in.grid(row=1)
+        self.vid_out_label = tk.Label(self.root, width=100, height=1, text="video output name (outputs to src/vid_out)", fg="black")
+        self.vid_out_label.grid(row=2)
+        self.vid_out = tk.Text(self.root, width=100, height=1)
+        self.vid_out.grid(row=3)
 
     def run(self):
         self.root.mainloop()
